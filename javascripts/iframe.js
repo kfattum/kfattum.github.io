be19565985e5a661c01fa828a0edda2a7e32c4b4
kfattum <fattum@3dpm.ru>
2023-09-21 12:07:13 +0300

    1

--- javascripts/iframe.js
+++
@@ -1,5 +0,0 @@
-window.onload = () => {
-    let iframeName = document.getElementById("comments-app-BV76JGsM-2");
-    let iframeContent = iframeName.contentDocument;
-    iframeContent.body.innerHTML = iframeContent.body.innerHTML + "<style>.tg-iframe{background-color: #161618}</style>";
-}
